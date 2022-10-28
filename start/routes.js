d5e0beac5f9f9b97f10b3ddc2db59e740ecd1edd
vicente Eduardo <vicentemanueleduardo@gmail.com>
2022-10-28 11:48:45 +0100

    2

--- start/routes.js
+++ start/routes.js
@@ -1,29 +1,39 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
+
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.on('/').render('welcome')
 Route.on('/singup').render('auth/singup')
-Route.on('/login').render('/auth/login')
-Route.post('/register','AuthController.singup')
-Route.post('login', 'AuthController.login').middleware('guest')
-Route.get('/logout', 'AuthController.logout').middleware('guest')
+
 
 //profile
 Route.on('/profile').render('profile')
 //Route.get('/profile','ProfileController.index').middleware('guest')
 
+//admin
+
+
+
+
+Route.get('/admin/painel','DashboardController.index').as('home.admin')
+
+//star users
+Route.get('users', 'UserController.index').as('users.index')
+Route.post('users', 'UserController.store').as('users.store')
+Route.get('users/create', 'UserController.create').as('users.create')
+Route.get('users/:id', 'UserController.show').as('users.show')
+Route.put('users/:id', 'UserController.update').as('users.update')
+Route.patch('users/:id', 'UserController.update')
+Route.get('users/:id/edit', 'UserController.edit').as('users.edit')
+Route.delete('users/:id', 'UserController.destroy').as('users.destroy')
+//and users
+
+//auth
+Route.on('/forgot-password').render('/auth/login')
+Route.on('/login').render('/auth/login')
+Route.post('/register','AuthController.singup')
+Route.post('login', 'AuthController.login').middleware('guest')
+Route.get('/logout', 'AuthController.logout').middleware('guest')
